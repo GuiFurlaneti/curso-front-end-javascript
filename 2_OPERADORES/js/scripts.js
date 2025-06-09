cc200f1a184728ce500a07413cf68d0983ad62ba
Guilherme Furlaneti <gfurlaneti@gmail.com>
2025-06-09 18:18:05 -0300

    att 09-06

--- 2_OPERADORES/js/scripts.js
+++ 2_OPERADORES/js/scripts.js
@@ -1,11 +1,42 @@
-// 1 - NUMBER
-console.log(typeof 2);
-console.log(typeof 5.14);
-console.log(typeof -127);
+// // 1 - NUMBER
+// console.log(typeof 2);
+// console.log(typeof 5.14);
+// console.log(typeof -127);
 
-// 2 - OPERAÇÕES ARITMÉTICAS
-console.log(2 + 4);
-console.log(10 - 5);
-console.log(5 / 2);
-console.log(7 * 2);
-console.log(5 + 4 * 2)
+// // 2 - OPERAÇÕES ARITMÉTICAS
+// console.log(2 + 4);
+// console.log(10 - 5);
+// console.log(5 / 2);
+// console.log(7 * 2);
+// console.log(5 + 4 * 2)
+
+// // 3 - SPECIAL NUMBERS
+// console.log(typeof Infinity);
+// console.log(typeof -Infinity);
+// console.log(12 * "asd");
+// console.log(typeof NaN);
+
+// // 4 - STRINGS
+// console.log("Um texto");
+// console.log('Mais um texto');
+// console.log('13');
+// console.log(typeof "Um texto");
+// console.log(typeof 'Mais um texto');
+
+// 5 - SIMBOLOS ESPECIAIS EM STRING
+console.log("Testando a \nquebra de linha");
+console.log("Espaçamento \tde tab");
+
+// 6 - CONCATENAÇÃO
+console.log("Oi, " + "tudo" + " bem?");
+console.log('Testando ' + 'com' + ' crase!');
+
+// 7 - TEMPLATE STRINGS
+console.log(`A soma de 2 + 2 é: ${2 + 2}`);
+console.log(`Podemos executar qualquer coisa aqui ${console.log("TESTE")}`);
+
+// 8 - BOOLEAN
+console.log(true);
+console.log(5 > 20);
+console.log(30 > 10);
+console.log(typeof true);
