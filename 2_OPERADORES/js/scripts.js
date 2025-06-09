c6a758153594aa5d66cdd3143313f60df88c243e
Guilherme Furlaneti <gfurlaneti@gmail.com>
2025-06-09 17:37:00 -0300

    Primeiro commit

---
+++ 2_OPERADORES/js/scripts.js
@@ -0,0 +1,11 @@
+// 1 - NUMBER
+console.log(typeof 2);
+console.log(typeof 5.14);
+console.log(typeof -127);
+
+// 2 - OPERAÇÕES ARITMÉTICAS
+console.log(2 + 4);
+console.log(10 - 5);
+console.log(5 / 2);
+console.log(7 * 2);
+console.log(5 + 4 * 2)
