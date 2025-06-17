3eaa75101c57249dfdc68f4eaf3aa5681c5ba4df
Guilherme Furlaneti <gfurlaneti@gmail.com>
2025-06-17 17:55:48 -0300

    att-17-06-24

--- 2_OPERADORES/js/scripts.js
+++ 2_OPERADORES/js/scripts.js
@@ -23,20 +23,41 @@
 // console.log(typeof "Um texto");
 // console.log(typeof 'Mais um texto');
 
-// 5 - SIMBOLOS ESPECIAIS EM STRING
-console.log("Testando a \nquebra de linha");
-console.log("Espaçamento \tde tab");
+// // 5 - SIMBOLOS ESPECIAIS EM STRING
+// console.log("Testando a \nquebra de linha");
+// console.log("Espaçamento \tde tab");
 
-// 6 - CONCATENAÇÃO
-console.log("Oi, " + "tudo" + " bem?");
-console.log('Testando ' + 'com' + ' crase!');
+// // 6 - CONCATENAÇÃO
+// console.log("Oi, " + "tudo" + " bem?");
+// console.log('Testando ' + 'com' + ' crase!');
 
-// 7 - TEMPLATE STRINGS
-console.log(`A soma de 2 + 2 é: ${2 + 2}`);
-console.log(`Podemos executar qualquer coisa aqui ${console.log("TESTE")}`);
+// // 7 - TEMPLATE STRINGS
+// console.log(`A soma de 2 + 2 é: ${2 + 2}`);
+// console.log(`Podemos executar qualquer coisa aqui ${console.log("TESTE")}`);
 
-// 8 - BOOLEAN
-console.log(true);
-console.log(5 > 20);
-console.log(30 > 10);
-console.log(typeof true);
+// // 8 - BOOLEAN
+// console.log(true);
+// console.log(5 > 20);
+// console.log(30 > 10);
+// console.log(typeof true);
+
+// // 9 - COMPARAÇÕES
+// console.log(5 <= 5);
+// console.log(5 < 5);
+// console.log(10 == 10);
+// console.log(10 == 9);
+// console.log(10 != 9);
+
+// // 10 - IDÊNTICO 
+// console.log( 9 == "9");
+// console.log(0 === "9");
+
+// 11 - OPERADORES LÓGICOS
+console.log(true && true);
+console.log(true && false);
+console.log(5 > 2 && 2 < 10);
+console.log(5 > 2 && "Gui" === 1);
+console.log(5 > 2 || "Gui" === 1);
+console.log(5 < 2 || 5 > 200);
+console.log(!true);
+console.log(!5 > 2);
